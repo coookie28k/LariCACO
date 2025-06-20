f1ecd91bd3efb1b2b55aa99f2373f1a37d433eba
Carolina <carolmomoli@outlook.com>
2025-06-20 10:57:55 -0300

    criacao da classe Usuario

---
+++ LariCACO/app/src/main/java/laricaco/Usuario.java
@@ -0,0 +1,50 @@
+package laricaco;
+
+public class Usuario {
+    private String login;
+    private String senha;
+    private double saldo;
+    private Carrinho carrinho;
+
+    Usuario(String login, String senha, double saldo, Carrinho carrinho) {
+        this.login = login;
+        this.senha = senha;
+        this.saldo = saldo;
+        this.carrinho = carrinho;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public boolean verificaSenha(String senha) {
+        if (senha == this.senha)
+            return true;
+        else
+            return false;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Carrinho getCarrinho() {
+        return carrinho;
+    }
+
+    public void setCarrinho(Carrinho carrinho) {
+        this.carrinho = carrinho;
+    }
+}
