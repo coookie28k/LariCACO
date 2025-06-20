17649e8aab8a42854db3c375f434860d6535ed38
Carolina <carolmomoli@outlook.com>
2025-06-20 11:33:57 -0300

    criacao da classe Vendedor

---
+++ LariCACO/app/src/main/java/laricaco/Vendedor.java
@@ -0,0 +1,43 @@
+package laricaco;
+
+import java.util.List;
+
+public class Vendedor extends Usuario {
+    private List<ItemVenda> minhasVendas;
+    private List<Produto> meusProdutos;
+
+    Vendedor(String login, String senha, double saldo, Carrinho carrinho, List<ItemVenda> minhasVendas,
+            List<Produto> meusProdutos) {
+        super(login, senha, saldo, carrinho);
+        this.minhasVendas = minhasVendas;
+        this.meusProdutos = meusProdutos;
+    }
+
+    public List<ItemVenda> getMinhasVendas() {
+        return minhasVendas;
+    }
+
+    public void setMinhasVendas(List<ItemVenda> minhasVendas) {
+        this.minhasVendas = minhasVendas;
+    }
+
+    public List<Produto> getMeusProdutos() {
+        return meusProdutos;
+    }
+
+    public void setMeusProdutos(List<Produto> meusProdutos) {
+        this.meusProdutos = meusProdutos;
+    }
+
+    public void adicionarProduto(Produto produto) {
+        this.meusProdutos.add(produto);
+    }
+
+    public void removerProduto(Produto produto) {
+        this.meusProdutos.remove(produto);
+    }
+
+    public void adicionarItemVenda(ItemVenda itemVenda) {
+        this.minhasVendas.add(itemVenda);
+    }
+}
