9761682467d4625ad80abff840513e751416f402
Carolina <carolmomoli@outlook.com>
2025-06-20 11:48:20 -0300

    criacao da classe ItemVenda

--- LariCACO/app/src/main/java/laricaco/ItemVenda.java
+++ LariCACO/app/src/main/java/laricaco/ItemVenda.java
@@ -1,5 +1,49 @@
 package laricaco;
 
-public class ItemVenda {
+import java.time.LocalDate;
 
+public class ItemVenda {
+    private LocalDate data;
+    private double total;
+    private Produto produto;
+    private int quantidade;
+
+    ItemVenda(LocalDate data, double total, Produto produto, int quantidade) {
+        this.data = data;
+        this.total = total;
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
 }
