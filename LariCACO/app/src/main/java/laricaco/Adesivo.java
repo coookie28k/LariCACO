79697b781e529c0899e9b51111f52d9d2194ce08
Carolina <carolmomoli@outlook.com>
2025-06-22 23:06:41 -0300

    comentarios da classe Adesivo

--- LariCACO/app/src/main/java/laricaco/Adesivo.java
+++ LariCACO/app/src/main/java/laricaco/Adesivo.java
@@ -3,16 +3,36 @@ package laricaco;
 public class Adesivo extends Produto {
     private String tamanho;
 
+    /**
+     * Construtor da classe adesivo
+     * 
+     * @param id        id do produto
+     * @param nome      nome do produto
+     * @param preco     preco do produto
+     * @param descricao descricao do produto
+     * @param estoque   quantidade em estoque
+     * @param tamanho   tamanho do adesivo
+     */
     public Adesivo(int id, String nome, double preco, String descricao, int estoque,
             String tamanho) {
         super(id, nome, preco, descricao, estoque);
         this.tamanho = tamanho;
     }
 
+    /**
+     * Retorna o tamanho do adesivo
+     * 
+     * @return tamanho do adesivo
+     */
     public String getTamanho() {
         return tamanho;
     }
 
+    /**
+     * Altera o tamanho do adesivo para tamanho
+     * 
+     * @param tamanho novo tamanho do adesivo
+     */
     public void setTamanho(String tamanho) {
         this.tamanho = tamanho;
     }
